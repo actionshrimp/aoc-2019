1420390a3d967effa543293a67b16b9b6558f4c8
Dave Aitken <dave.aitken@gmail.com>
2019-12-02 18:25:50 +0000

    ocaml: day2 part 1

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,3 +1,15 @@
+let print_day ~day ~part_1 ?part_2 () =
+  print_endline (Printf.sprintf "Day %d" day) ;
+  print_endline (Printf.sprintf "Part 1: %d" part_1) ;
+  match part_2 with
+  | None ->
+      ()
+  | Some p2 ->
+      print_endline (Printf.sprintf "Part 2: %d" p2)
+
+
+(* module type Aoc_day.................... *)
+
 module Day1 = struct
   let part_1 () =
     Aoc2019.Util.fold_file_lines
@@ -18,9 +30,60 @@ module Day1 = struct
       0
 
 
-  let run () =
-    print_endline (Printf.sprintf "Part 1: %d" (part_1 ())) ;
-    print_endline (Printf.sprintf "Part 2: %d" (part_2 ()))
+  let run () = print_day ~day:1 ~part_1:(part_1 ()) ~part_2:(part_2 ()) ()
 end
 
-let () = Day1.run ()
+module Day2 = struct
+  let rec run_intcode pos memory =
+    let code = memory.(pos) in
+    if code == 1
+    then (
+      print_endline "opcode 1" ;
+      let loc_1 = memory.(pos + 1) in
+      let loc_2 = memory.(pos + 2) in
+      let result = memory.(loc_1) + memory.(loc_2) in
+      let out = memory.(pos + 3) in
+      memory.(out) <- result ;
+      run_intcode (pos + 4) memory )
+    else if code == 2
+    then (
+      print_endline "opcode 2" ;
+      let loc_1 = memory.(pos + 1) in
+      let loc_2 = memory.(pos + 2) in
+      let result = memory.(loc_1) * memory.(loc_2) in
+      let out = memory.(pos + 3) in
+      memory.(out) <- result ;
+      run_intcode (pos + 4) memory )
+    else if code == 99
+    then
+      let _ = print_endline "opcode 99" in
+      memory
+    else failwith (Printf.sprintf "unexpected opcode: %d" code)
+
+
+  let set_gravity_assist_memory memory =
+    memory.(1) <- 12 ;
+    memory.(2) <- 2
+
+
+  let part_1 () =
+    let input =
+      Aoc2019.Util.fold_file_lines
+        "../rust/input/2019/day2.txt"
+        (fun _acc line -> line (* there's only one line in the file *))
+        ""
+    in
+    let memory =
+      Tablecloth.String.split ~on:"," input
+      |> Tablecloth.List.map ~f:int_of_string
+      |> Tablecloth.Array.from_list
+    in
+    set_gravity_assist_memory memory ;
+    let memory' = run_intcode 0 memory in
+    memory'.(0)
+
+
+  let run () = print_day ~day:2 ~part_1:(part_1 ()) ()
+end
+
+let () = Day2.run ()
