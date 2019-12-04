cb118bf1ba8f205c699b5f3c893d0db7db266192
Dave Aitken <dave.aitken@gmail.com>
2019-12-04 11:06:01 +0000

    day 3 part 1 (ocaml)

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -101,4 +101,120 @@ module Day2 = struct
   let run () = print_day ~day:2 ~part_1:(part_1 ()) ~part_2:(part_2 ()) ()
 end
 
-let () = Day2.run ()
+module Day3 = struct
+  type direction =
+    | Left
+    | Right
+    | Up
+    | Down
+
+  type move = direction * int
+
+  let move (x, y) = function
+    | Left, d ->
+        (x - d, y)
+    | Right, d ->
+        (x + d, y)
+    | Up, d ->
+        (x, y - d)
+    | Down, d ->
+        (x, y + d)
+
+
+  let intersections init_as init_bs =
+    let rec run_intersections (a_x, a_y) (b_x, b_y) as_ bs =
+      match (as_, bs) with
+      | [], _ ->
+          []
+      | a :: other_as, [] ->
+          run_intersections (move (a_x, a_y) a) (0, 0) other_as init_bs
+      | (a_dir, a_d) :: _other_as, (b_dir, b_d) :: other_bs ->
+          let within dx d = dx > 0 && dx < d in
+          let intersec =
+            match (a_dir, b_dir) with
+            | Up, Left when within (b_x - a_x) b_d && within (a_y - b_y) a_d ->
+                Some (a_x, b_y)
+            | Up, Right when within (a_x - b_x) b_d && within (a_y - b_y) a_d
+              ->
+                Some (a_x, b_y)
+            | Down, Left when within (b_x - a_x) b_d && within (b_y - a_y) a_d
+              ->
+                Some (a_x, b_y)
+            | Down, Right when within (a_x - b_x) b_d && within (b_y - a_y) a_d
+              ->
+                Some (a_x, b_y)
+            | Left, Up when within (a_x - b_x) a_d && within (b_y - a_y) b_d ->
+                Some (b_x, a_y)
+            | Left, Down when within (a_x - b_x) a_d && within (a_y - b_y) b_d
+              ->
+                Some (b_x, a_y)
+            | Right, Up when within (b_x - a_x) a_d && within (b_y - a_y) b_d
+              ->
+                Some (b_x, a_y)
+            | Right, Down when within (b_x - a_x) a_d && within (a_y - b_y) b_d
+              ->
+                Some (b_x, a_y)
+            | _ ->
+                None
+          in
+          T.Option.to_list intersec
+          @ run_intersections
+              (a_x, a_y)
+              (move (b_x, b_y) (b_dir, b_d))
+              as_
+              other_bs
+    in
+    run_intersections (0, 0) (0, 0) init_as init_bs
+
+
+  let distance (x, y) = Pervasives.abs x + Pervasives.abs y
+
+  let example_1 () =
+    intersections
+      [ (Right, 8); (Up, 5); (Left, 5); (Down, 3) ]
+      [ (Up, 7); (Right, 6); (Down, 4); (Left, 4) ]
+    |> T.List.map ~f:distance
+    |> T.List.minimum
+    |> T.Option.getExn
+
+
+  let parse_line line =
+    line
+    |> T.String.split ~on:","
+    |> T.List.map ~f:(fun part ->
+           match T.String.toList part with
+           | 'R' :: d ->
+               (Right, int_of_string (T.String.fromList d))
+           | 'L' :: d ->
+               (Left, int_of_string (T.String.fromList d))
+           | 'U' :: d ->
+               (Up, int_of_string (T.String.fromList d))
+           | 'D' :: d ->
+               (Down, int_of_string (T.String.fromList d))
+           | c :: _ ->
+               failwith (Printf.sprintf "unexpected direction in input: %c" c)
+           | [] ->
+               failwith "empty input")
+
+
+  let part_1 () =
+    let lines =
+      Aoc2019.Util.fold_file_lines
+        "../rust/input/2019/day3.txt"
+        (fun acc line -> acc @ [ parse_line line ])
+        []
+    in
+    match lines with
+    | [ a; b ] ->
+        intersections a b
+        |> T.List.map ~f:distance
+        |> T.List.minimum
+        |> T.Option.getExn
+    | _ ->
+        failwith "wrong number of lines in input"
+
+
+  let run () = print_day ~day:3 ~part_1:(part_1 ()) ()
+end
+
+let () = Day3.run ()
